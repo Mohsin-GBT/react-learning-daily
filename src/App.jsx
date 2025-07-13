04c409bd83d24514091a5c2681d34c4459ce2855
mohsin7786 <122112047+mohsin7786@users.noreply.github.com>
2025-07-13 15:34:35 +0530

    Day 1: React props basics

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <>
+ <Header/>
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react'
+
+// const App = () => {
+//   const [username, setusername] = useState('');
+//   const submithandler = (e) => {
+//     e.preventDefault()
+//     console.log(username)
+//     setusername('')
+//   }
+//   // const changeUser = () => {
+//   //   setuser('user2')
+//   // }
+//   return (
+//     <div className="container">
+//       {/* <h1>user name is {user}</h1>
+//       <button onClick={changeUser}>change</button>
+//       <h1 class="text-3xl font-bold underline">
+//         Hello world!
+//       </h1> */}
+//       <form onSubmit={(e) => {
+//         submithandler(e)
+//       }}>
+//         <input value={username} onChange={(e) => {
+//           setusername(e.target.value)
+//         }} type="text" className='px-4 py-3 rounded text-xl m-5 bg-white text-black' />
+//         <button className="px-4 text-xl bg-emerald-600">Submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
